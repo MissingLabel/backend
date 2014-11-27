f92f57fe9e3c6dd46bb9dae04031a3cd5483fc41
johnw335 <jww335@gmail.com>
2014-11-27 11:20:23 -0600

    added plu parcer and plu excel

---
+++ db/plu_parcer.rb
@@ -0,0 +1,24 @@
+# In this example the model MyFile has_attached_file :attachment
+def parce_plu_excel
+  Spreadsheet.client_encoding = 'UTF-8'
+  @workbook = Spreadsheet.open('../fixtures/plu_spreadsheet.xlsx')
+
+  # Get the first worksheet in the Excel file
+  @worksheet = @workbook.worksheet(0)
+
+  # It can be a little tricky looping through the rows since the variable
+  # @worksheet.rows often seem to be empty, but this will work:
+  0.upto @worksheet.last_row_index do |index|
+    # .row(index) will return the row which is a subclass of Array
+    row = @worksheet.row(index)
+
+    @produce = ProduceByPlu.new
+    #row[0] is the first cell in the current row, row[1] is the second cell, etc...
+    @produce.plu_number = row[0]
+    @produce.commodity = row[1]
+    @produce.variety = row[2]
+    @produce.size = row[3]
+
+    @produce.save
+  end
+end
