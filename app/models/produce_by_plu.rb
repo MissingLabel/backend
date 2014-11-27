4634575157d2861a02b4e27614203f9b7ca74576
Gale Van Rossem & Shreya Patel <pair+galeforcevr.shreya317@devbootcamp.com>
2014-11-27 11:23:57 -0600

    added models and migrations

---
+++ app/models/produce_by_plu.rb
@@ -0,0 +1,3 @@
+class ProduceByPlu < ActiveRecord::Base
+  has_many :produce_by_gs1s
+end
---
+++ db/migrate/20141127164215_create_produce_by_plus.rb
@@ -0,0 +1,14 @@
+class CreateProduceByPlus < ActiveRecord::Migration
+  def change
+    create_table :produce_by_plus do |t|
+      t.integer :plu_number
+      t.string :commodity
+      t.string :variety
+      t.string :size
+      t.text :how_to_store
+      t.text :how_to_select
+
+      t.timestamps
+    end
+  end
+end
