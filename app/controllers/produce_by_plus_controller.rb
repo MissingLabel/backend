ddc7da81145bdd48282995f3587c2356c449d46c
Shreya Patel <shreya317@gmail.com>
2014-11-28 15:06:41 -0600

    added params to produce plu controller

--- app/controllers/produce_by_plus_controller.rb
+++ app/controllers/produce_by_plus_controller.rb
@@ -1,7 +1,7 @@
 class ProduceByPlusController < ApplicationController
 
   def show
-    @produce_item = ProduceByPlu.find_by(plu_number: 4067)#params[:plu_num])
+    @produce_item = ProduceByPlu.find_by(plu_number: params[:plu_num])
     @json_produce_item = {commodity: @produce_item.commodity,
                           variety: @produce_item.variety}.to_json
   end
