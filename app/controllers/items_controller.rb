3d4f6ba3f679b0c04711bf961986c9aa69446d56
Shreya Patel <shreya317@gmail.com>
2014-11-28 16:02:59 -0600

    added access-control-allow-credentials

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,8 @@ class ItemsController < ApplicationController
 
   def json
     @produce_item = ProduceByPlu.find_by(plu_number: params[:plu_number])
-    @json_produce_item = {commodity: @produce_item.commodity,
+    @json_produce_item = {Access-Control-Allow-Credentials: true,
+                          commodity: @produce_item.commodity,
                           variety: @produce_item.variety}.to_json
   end
 
