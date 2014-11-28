d38d1567fd666e3c265a27db7c737a6854e22252
Shreya Patel <shreya317@gmail.com>
2014-11-28 16:15:17 -0600

    Access-Control-Allow-Origin

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,6 +3,7 @@ class ItemsController < ApplicationController
   def json
     @produce_item = ProduceByPlu.find_by(plu_number: params[:plu_number])
     @json_produce_item = {"Access-Control-Allow-Credentials" => true,
+                          "Access-Control-Allow-Origin" => true,
                           commodity: @produce_item.commodity,
                           variety: @produce_item.variety}.to_json
   end
