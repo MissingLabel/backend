031421f2d0615f520b5d4350211d3d80a824bab0
Shreya Patel <shreya317@gmail.com>
2014-11-28 20:11:15 -0600

    modified json format

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,14 +2,26 @@ class ItemsController < ApplicationController
   
   protect_from_forgery except: :json_object
   
-  def json_object
+  # def json_object
 
     # if request.xhr?
-      @produce_item = ProduceByPlu.find_by(plu_number: params[:plu_number])
-      @json_produce_item = {commodity: @produce_item.commodity,
-                            variety: @produce_item.variety}.to_json
+      # @produce_item = ProduceByPlu.find_by(plu_number: params[:plu_number])
+      # render :json => {commodity: @produce_item.commodity, variety: @produce_item.variety}.to_json
+
     # end
 
+  def json_object
+    @produce_item = ProduceByPlu.find_by(plu_number: params[:plu_number])        respond_to do |format|
+      format.html # index.html.erb
+      format.json { render :json => @produce_item }
+    end
   end
 
+  # end
+
 end
+
+
+
+      
+    
