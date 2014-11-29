be8d845a58392c60c84cc79b1cf9d61ac3443980
Shreya Patel <shreya317@gmail.com>
2014-11-28 20:14:21 -0600

    modified items controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,17 @@ class ItemsController < ApplicationController
   
   protect_from_forgery except: :json_object
   
+  def json_object
+    @produce_item = ProduceByPlu.find_by(plu_number: params[:plu_number])
+    respond_to do |format|
+      format.json { render :json => @produce_item }
+    end
+  end
+
+end
+
+
+
   # def json_object
 
     # if request.xhr?
@@ -10,18 +21,4 @@ class ItemsController < ApplicationController
 
     # end
 
-  def json_object
-    @produce_item = ProduceByPlu.find_by(plu_number: params[:plu_number])        respond_to do |format|
-      format.html # index.html.erb
-      format.json { render :json => @produce_item }
-    end
-  end
-
-  # end
-
-end
-
-
-
-      
     
