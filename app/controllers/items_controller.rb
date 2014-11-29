617a9ec1b2b17f7f5f7fccbb18cd8d3856072054
Shreya Patel <shreya317@gmail.com>
2014-11-28 19:25:53 -0600

    added if request.xhr to items controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,9 +4,12 @@ class ItemsController < ApplicationController
   
   def json_object
 
-    @produce_item = ProduceByPlu.find_by(plu_number: params[:plu_number])
-    @json_produce_item = {commodity: @produce_item.commodity,
-                          variety: @produce_item.variety}.to_json
+    if request.xhr?
+      @produce_item = ProduceByPlu.find_by(plu_number: params[:plu_number])
+      @json_produce_item = {commodity: @produce_item.commodity,
+                            variety: @produce_item.variety}.to_json
+    end
+
   end
 
 end
