a0493910ce55caf640c33eee575325ff65104bcc
Shreya Patel <shreya317@gmail.com>
2014-12-01 07:47:36 -0600

    add api back

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,20 +6,20 @@ class ItemsController < ApplicationController
     print params
     @number = params[:number]
 
-    @produce_item = ProduceByPlu.find_by(plu_number: @number)
+    # @produce_item = ProduceByPlu.find_by(plu_number: @number)
 
-    # if @number.length < 6
-    #   @plu_number = params[:number]
-    # else
-    #   @gs1_number = params[:number]
-    # end
+    if @number.length < 6
+      @plu_number = params[:number]
+    else
+      @gs1_number = params[:number]
+    end
 
-    # @item = plu_or_gs1_item(@plu_number, @gs1_number)
+    @item = plu_or_gs1_item(@plu_number, @gs1_number)
 
-    # nutrition = NutritionApi.new(@item.ndb_no)
-    # @produce_item = nutrition.prettify_api_info
+    nutrition = NutritionApi.new(@item.ndb_no)
+    @produce_item = nutrition.prettify_api_info
 
-    # @produce_item = organic_or_gmo(@number, @produce_item)
+    @produce_item = organic_or_gmo(@number, @produce_item)
 
 
     respond_to do |format|
