55f51b3830d757dc0267b1c0f525ccd1d00e467e
Shreya Patel <shreya317@gmail.com>
2014-11-30 21:18:56 -0600

    added back item helper

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,34 +11,13 @@ class ItemsController < ApplicationController
       @gs1_number = params[:number]
     end
 
-    # @item = plu_or_gs1_item(@plu_number, @gs1_number)
-
-    if @plu_number
-      if @plu_number.length == 5
-        @plu_number = @plu_number[1..-1]
-      end
-      @item = ProduceByPlu.find_by(plu_number: @plu_number)
-    else
-      @item = ProduceByGs1.find_by(gs1_number: @gs1_number).produce_by_plu
-    end
+    @item = plu_or_gs1_item(@plu_number, @gs1_number)
 
     nutrition = NutritionApi.new(@item.ndb_no)
     @produce_item = nutrition.prettify_api_info
 
-    # @produce_item = organic_or_gmo(@number, @produce_item)
+    @produce_item = organic_or_gmo(@number, @produce_item)
 
-    if @plu_number.length == 5
-      if @plu_number[0] == 8
-        @produce_item[:gmo] = "True"
-        @produce_item[:oraganic] = "False"
-      else @plu_number[0] == 9
-        @produce_item[:gmo] = "False"
-        @produce_item[:oraganic] = "True"
-      end
-    else
-      @produce_item[:gmo] = "False"
-      @produce_item[:oraganic] = "False"
-    end
 
     respond_to do |format|
       format.json { render :json => @produce_item }
--- app/helpers/item_helper.rb
+++ app/helpers/item_helper.rb
@@ -1,30 +1,30 @@
-module ItemsHelper
+module ItemHelper
 
-  # def plu_or_gs1_item(@plu_number, @gs1_number)
-  #   if @plu_number
-  #     if @plu_number.length == 5
-  #       @plu_number = @plu_number[1..-1]
-  #     end
-  #     item = ProduceByPlu.find_by(plu_number: @plu_number)
-  #   else
-  #     item = ProduceByGs1.find_by(gs1_number: @gs1_number).produce_by_plu
-  #   end
-  #   item
-  # end
+  def plu_or_gs1_item(@plu_number, @gs1_number)
+    if @plu_number
+      if @plu_number.length == 5
+        @plu_number = @plu_number[1..-1]
+      end
+      item = ProduceByPlu.find_by(plu_number: @plu_number)
+    else
+      item = ProduceByGs1.find_by(gs1_number: @gs1_number).produce_by_plu
+    end
+    item
+  end
 
-  # def organic_or_gmo(@plu_number, @produce_item)
-  #   if @plu_number.length == 5
-  #     if @plu_number[0] == 8
-  #       @produce_item[:gmo] = "True"
-  #       @produce_item[:oraganic] = "False"
-  #     else @plu_number[0] == 9
-  #       @produce_item[:gmo] = "False"
-  #       @produce_item[:oraganic] = "True"
-  #     end
-  #   else
-  #     @produce_item[:gmo] = "False"
-  #     @produce_item[:oraganic] = "False"
-  #   end
-  #   @produce_item
-  # end
+  def organic_or_gmo(@plu_number, @produce_item)
+    if @plu_number.length == 5
+      if @plu_number[0] == 8
+        @produce_item[:gmo] = "True"
+        @produce_item[:oraganic] = "False"
+      else @plu_number[0] == 9
+        @produce_item[:gmo] = "False"
+        @produce_item[:oraganic] = "True"
+      end
+    else
+      @produce_item[:gmo] = "False"
+      @produce_item[:oraganic] = "False"
+    end
+    @produce_item
+  end
 end
