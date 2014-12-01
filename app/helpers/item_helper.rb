c253f5faad1e733884d7fcf0d62232ff9f39d8a0
Shreya Patel <shreya317@gmail.com>
2014-11-30 21:24:17 -0600

    removed instance variables from item helper

--- app/helpers/item_helper.rb
+++ app/helpers/item_helper.rb
@@ -1,6 +1,6 @@
 module ItemHelper
 
-  def plu_or_gs1_item(@plu_number, @gs1_number)
+  def plu_or_gs1_item(plu_number, gs1_number)
     if @plu_number
       if @plu_number.length == 5
         @plu_number = @plu_number[1..-1]
@@ -12,7 +12,7 @@ module ItemHelper
     item
   end
 
-  def organic_or_gmo(@plu_number, @produce_item)
+  def organic_or_gmo(plu_number, produce_item)
     if @plu_number.length == 5
       if @plu_number[0] == 8
         @produce_item[:gmo] = "True"
