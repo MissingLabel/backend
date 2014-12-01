bf9df079d34993954d45b5e93ebe29f4ff443a04
Shreya Patel <shreya317@gmail.com>
2014-12-01 14:12:21 -0600

    ordered excel alphabetically

--- db/plu_parcer.rb
+++ db/plu_parcer.rb
@@ -8,7 +8,7 @@ def parce_plu_excel
 
   # It can be a little tricky looping through the rows since the variable
   # @worksheet.rows often seem to be empty, but this will work:
-  0.upto @worksheet.last_row_index do |index|
+  1.upto @worksheet.last_row_index do |index|
     # .row(index) will return the row which is a subclass of Array
     row = @worksheet.row(index)
 
