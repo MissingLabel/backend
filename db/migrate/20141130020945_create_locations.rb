13f1d3ec4713a80d3cb84134c58661732447f228
johnw335 <jww335@gmail.com>
2014-11-30 18:13:29 -0600

    fixed some bugs in seeds and added the location info

--- db/migrate/20141130020945_create_locations.rb
+++ db/migrate/20141130020945_create_locations.rb
@@ -2,7 +2,8 @@ class CreateLocations < ActiveRecord::Migration
   def change
     create_table :locations do |t|
       t.belongs_to :produce_by_gs1
-
+      t.string :name
+      t.string :address
       t.timestamps
     end
   end
