262d86576e4aed08249642358e6e8246211e96fd
Shreya Patel <shreya317@gmail.com>
2014-12-01 14:39:12 -0600

    fixed bug in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -160,7 +160,7 @@ commodities.each do |commodity|
   end
   fall_list = Seasonal_list.new("http://www.fruitsandveggiesmorematters.org/whats-in-season-fall")
   if fall_list.list.include?(commodity)
-    produce = ProduceByPlu.where(commodi ty: "#{commodity}")
+    produce = ProduceByPlu.where(commodity: "#{commodity}")
     produce.each do |item|
       item.seasons << Season.find(4)
     end
