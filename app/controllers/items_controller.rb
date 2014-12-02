dd20c81eb3941c2b5c4ab125d77fa2568e1fea85
Shreya Patel <shreya317@gmail.com>
2014-12-01 22:17:01 -0600

    added else condition for invalid input

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -14,16 +14,20 @@ class ItemsController < ApplicationController
       @item = ProduceByGs1.find_by(gs1_number: @gs1_number).produce_by_plu
     end
 
-    nutrition = NutritionApi.new(@item.ndb_no)
-    @produce_item = nutrition.prettify_api_info
+    if @item == nil
+      @produce_item = "Invalid input "
+    else
+      nutrition = NutritionApi.new(@item.ndb_no)
+      @produce_item = nutrition.prettify_api_info
 
-    @produce_item = organic_or_gmo(@number, @produce_item)
+      @produce_item = organic_or_gmo(@number, @produce_item)
 
-    @produce_item[:plu_no] = @plu_number
-    @produce_item[:variety] = @item.variety\
-    #comment back in when finishd testing gs1 numbers
-    #@produce_item[:farm_geo_location] = farm_geo_api(@gs1_number.location.address) if @gs1_number
-    @produce_item[:farm_geo_location] = "2799+N.+1700+East+Rd.+Martinton,+IL+60951"
+      @produce_item[:plu_no] = @plu_number
+      @produce_item[:variety] = @item.variety\
+      #comment back in when finishd testing gs1 numbers
+      #@produce_item[:farm_geo_location] = farm_geo_api(@gs1_number.location.address) if @gs1_number
+      @produce_item[:farm_geo_location] = "2799+N.+1700+East+Rd.+Martinton,+IL+60951"
+    end
 
     render :json => @produce_item
   end
