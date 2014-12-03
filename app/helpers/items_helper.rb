039e210e036664643923a50af84d4ffce2c460dc
johnw335 <jww335@gmail.com>
2014-12-03 14:30:35 -0600

    fixed bug in items helper method

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -11,19 +11,19 @@ module ItemsHelper
   end
 
   def organic_or_gmo(plu_number, produce_item)
-    if @plu_number.length == 5
-      if @plu_number[0] == 8
-        @produce_item[:gmo] = "True"
-        @produce_item[:oraganic] = "False"
-      else @plu_number[0] == 9
-        @produce_item[:gmo] = "False"
-        @produce_item[:oraganic] = "True"
+    if plu_number.length == 5
+      if plu_number[0] == 8
+        produce_item[:gmo] = "True"
+        produce_item[:oraganic] = "False"
+      else plu_number[0] == 9
+        produce_item[:gmo] = "False"
+        produce_item[:oraganic] = "True"
       end
     else
-      @produce_item[:gmo] = "False"
-      @produce_item[:oraganic] = "False"
+      produce_item[:gmo] = "False"
+      produce_item[:oraganic] = "False"
     end
-    @produce_item
+    produce_item
   end
 
   def farm_geo_api(farm_address)
