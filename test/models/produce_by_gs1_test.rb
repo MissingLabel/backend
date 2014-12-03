1f6305c9037d7eec84a18aca78aa4636d79d972e
johnw335 <jww335@gmail.com>
2014-12-03 10:24:16 -0600

    deleted test directory

--- test/models/produce_by_gs1_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class ProduceByGs1Test < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/seasonal_fruit_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class SeasonalFruitTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
