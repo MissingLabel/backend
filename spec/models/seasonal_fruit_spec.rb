f22e4e68052898574c5f32b3a17c966ef32afeb6
Shreya Patel <shreya317@gmail.com>
2014-12-03 20:04:33 -0600

    changed new to create line 12

--- spec/models/seasonal_fruit_spec.rb
+++ spec/models/seasonal_fruit_spec.rb
@@ -9,18 +9,16 @@ describe Season do
   									size: 'large', 
   									how_to_store: "Refrigerate apples in plastic bag away from stron...", 
   									how_to_select: " Choose firm, shiny, smooth-skinned apples with in...")}
-  let(:seasonal_apple) {SeasonalFruit.new(season: winter, produce_by_plu: apple)}
+  let(:seasonal_apple) {SeasonalFruit.create!(season: winter, produce_by_plu: apple)}
 
 
   context "#initialize" do
 
     it 'should belong to a Season' do
-      seasonal_apple.save!
       expect(seasonal_apple.season).to be_an_instance_of Season
   	end
 
   	it 'should belong to a ProduceByPlu' do
-      winter.save!
       expect(seasonal_apple.produce_by_plu).to be_an_instance_of ProduceByPlu
   	end
   end
