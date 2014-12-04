64b1380fe24eed5e33632180b4394253ebad118d
Shreya Patel <shreya317@gmail.com>
2014-12-03 20:01:08 -0600

    added SeasonalFruit model tests

---
+++ spec/models/seasonal_fruit_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+describe Season do
+  let(:winter) { Season.create!(name: "Winter") }
+  let(:apple)  	{ProduceByPlu.create!(plu_number: 3283, 
+  									ndb_no: "09003", 
+  									commodity: "apples", 
+  									variety: "honeycrisp", 
+  									size: 'large', 
+  									how_to_store: "Refrigerate apples in plastic bag away from stron...", 
+  									how_to_select: " Choose firm, shiny, smooth-skinned apples with in...")}
+  let(:seasonal_apple) {SeasonalFruit.new(season: winter, produce_by_plu: apple)}
+
+
+  context "#initialize" do
+
+    it 'should belong to a Season' do
+      seasonal_apple.save!
+      expect(seasonal_apple.season).to be_an_instance_of Season
+  	end
+
+  	it 'should belong to a ProduceByPlu' do
+      winter.save!
+      expect(seasonal_apple.produce_by_plu).to be_an_instance_of ProduceByPlu
+  	end
+  end
+
+end
